a6638ea995460742005494534e1870c382e9b202
Pranay Gurav <guravpranay81@gmail.com>
2026-01-30 13:49:12 +0530

    feat : navbar added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+export default function Hero(){
+    return (
+        <section className="py-24">
+            <div className="max-w-7xl mx-auto px-8 grid grid-cols-2 gap-16 items-center">
+                <div>
+                    <h1 className="text-5xl font-bold leading-tight mb-6 text-[#0F172A]">
+                        Workflows that actually move forward
+                    </h1>
+                    <p className="text-lg text-[#64748B] mb-8 max-w-lg">Plan ,track and deliver projects without chaos.
+                        FlowDesk helps your team stay aligned and focused.
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+export default function Navbar() {
+  return (
+    <header className="border-b border-slate-100">
+      <div className="max-w-7xl mx-auto px-8 h-16 flex items-center justify-between">
+
+        
+        <div className="text-xl font-bold text-[#2563EB]">
+          FlowDesk
+        </div>
+
+        
+        <nav className="hidden md:flex items-center gap-8 text-sm font-medium text-slate-600">
+          <a href="#" className="hover:text-[#0F172A]">Product</a>
+          <a href="#" className="hover:text-[#0F172A]">Features</a>
+          <a href="#" className="hover:text-[#0F172A]">Pricing</a>
+          <a href="#" className="hover:text-[#0F172A]">Docs</a>
+        </nav>
+
+        
+        <div className="flex items-center gap-4 text-sm font-medium">
+          <button className="text-slate-600 cursor-pointer hover:text-[#0F172A]">
+            Login
+          </button>
+
+          <button className="bg-[#2563EB] text-white px-5 py-2 rounded-md hover:opacity-90 transition">
+            Get Started
+          </button>
+        </div>
+
+      </div>
+    </header>
+  );
+}
